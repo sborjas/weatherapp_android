75f26e045218d43762ec651cc8ab46e8215abf7f
sborjas <iborjas@unitec.edu>
2018-08-20 21:44:51 -0600

    lkjlkj

---
+++ components/SearchInput.js
@@ -0,0 +1,65 @@
+import React from 'react';
+import { StyleSheet, TextInput, View } from 'react-native';
+
+ export default class SearchInput extends React.Component {
+
+    constructor(props) {
+        super(props);
+        this.state = {
+        text: '',
+        };
+        }
+
+        handleChangeText = text => {
+            this.setState({ text });
+        };
+        
+        handleSubmitEditing = () => {
+            const { onSubmit } = this.props;
+            const { text } = this.state;
+            if (!text) return;
+            onSubmit(text);
+            this.setState({ text: '' });
+            };
+
+    render() {
+        const { placeholder } = this.props;
+        const { text } = this.state;
+        return (
+            <View style={styles.container}>
+                <TextInput
+                    autoCorrect={false}
+                    placeholder={this.props.placeholder}
+                    placeholderTextColor="white"
+                    underlineColorAndroid="transparent"
+                    style={styles.textInput}
+                    clearButtonMode="always"
+                    onChangeText={this.handleChangeText}
+                    onSubmitEditing={this.handleSubmitEditing}
+                />
+            </View>
+        );
+    }
+ }
+
+const styles = StyleSheet.create({
+    container: {
+        
+        height: 40,
+        backgroundColor: '#666',
+        marginHorizontal: 40,
+        paddingHorizontal: 10,
+        borderRadius: 5,
+ },
+ textInput: {
+    backgroundColor: '#666',
+    color: 'white',
+    height: 40,
+    width: 250,
+    marginTop: 10,
+    marginHorizontal: 20,
+    paddingHorizontal: 10,
+    alignSelf: 'center',
+        
+    },
+});
